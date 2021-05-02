6bc953e2b23ea318c990163ee604c8d66ab0ea30
Jules Rufo <julesreegorufo@gmail.com>
2021-05-02 21:16:59 +0800

    Rewrite and follow steps in OTP from step 4

--- scripts.js
+++ scripts.js
@@ -11,8 +11,8 @@ function multiply(x, y) {
 }
 
 function divide(x, y) {
-    if(y == 0 ){
-        return "Syntax Error"
+    if(y == 0){
+        return "Syntax Error";
     }
 
     return x / y;
@@ -34,129 +34,106 @@ function operate(operation, x, y) {
 }
 
 function animateClick(element) {
-    element.classList.add('clicked');
+    element.classList.add('clicked')
+
     setTimeout(() => {
         element.classList.remove('clicked');
-    }, 100)
+    },100);
 }
 
-window.addEventListener('load', function() {
+window.addEventListener('load', () => {
     const numbers = document.querySelectorAll('.numbers');
     const display = document.querySelector('#display > p');
-    const backspace = document.getElementById('backspace');
     const operators = document.querySelectorAll('.operators');
-    let operation = "";
-    let chosenOperation = "";
+    const equals = document.getElementById('equals');
+    const clear = document.getElementById('clear');
     let num = 0;
     let firstNum = 0;
     let answer = 0;
-        
-    
+    let operation = "";
+
     numbers.forEach(number => {
         number.addEventListener('click', () => {
-            //max display length of 15
-            if(num.toString().length  == 15) { 
-                return;
-            }
-            //Check if operation has value
-            if(operation != ""){
-                chosenOperation = operation;
-                firstNum = display.textContent;
-                num = 0;
-                operation = "";
+            //check if operation has value. If true, clear current display
+            if(operation != ''){
+                display.textContent = '';
             }
 
-            //not 0 and no dot
-            if(num != 0 && !display.textContent.includes('.')) { 
-                num = num * 10 + parseInt(number.textContent,10);
-            }
-            //display has 0
-            else if(display.textContent.includes('.')) { 
-                if(num.toString().includes('.')){
-                    return;
-                }
-                num = ((num * 10) + parseInt(number.textContent,10)) / 10;
-            }
-            //display is 0
-            else { 
-                num = parseInt(number.textContent, 10);
-            }
-            display.textContent = num.toString();
+            num = (num * 10) + parseInt(number.textContent, 10);
+            display.textContent = num;
+
             animateClick(number);
         });
     });
 
     operators.forEach(operator => {
         operator.addEventListener('click', () => {
-            //Check if chosenOperation has already a value
-            if(chosenOperation != "" && operator.textContent != '=') {
-                firstNum = parseInt(firstNum, 10);
-                num = parseInt(num, 10);
-                answer = operate(chosenOperation, firstNum, num);
-                firstNum = answer;
-                display.textContent = answer;
+            //evaluate when there's an operation already. Otherwise, put value of num to firstNum
+            if(operation != '') {
+                firstNum = operate(operation, firstNum, num);
+                if(firstNum%1 != 0){ //if there's a remainder
+                    firstNum = firstNum.toFixed(2);
+                }
+                display.textContent = firstNum;
+                console.log(firstNum);
             }
+            else{
+                firstNum = num;
+            }
+            
+            //reset num to zero
+            num = 0;
+
+            
 
             if(operator.textContent == '+') {
                 operation = 'add';
             }
-            else if (operator.textContent == '-') {
+            else if(operator.textContent == '-') {
                 operation = 'subtract';
             }
-            else if (operator.textContent == 'x') {
+            else if(operator.textContent == 'x') {
                 operation = 'multiply';
             }
-            else if (operator.textContent == '/') {
+            else if(operator.textContent == '/') {
                 operation = 'divide';
-            }
-            else if (operator.textContent == 'C') {
-                num = 0;
-                display.textContent = 0;
-                animateClick(operator);
-            }
-            else if (operator.textContent == '.') {
-                if(display.textContent.toString().includes('.')) {
-                    return;
-                }
-        
-                display.textContent += '.';
-                animateClick(operator);
-            }
-            else if (operator.textContent == '=') {
-                firstNum = parseInt(firstNum, 10);
-                num = parseInt(num, 10);
-                if(chosenOperation == 'add'){
-                    answer = operate('add', firstNum, num);
-                }
-                else if(chosenOperation == 'subtract'){
-                    answer = operate('subtract', firstNum, num);
-                }
-                else if(chosenOperation == 'multiply'){
-                    answer = operate('multiply', firstNum, num);
-                }
-                else if(chosenOperation == 'divide'){
-                    answer = operate('divide', firstNum, num);
-                }
-
-                display.textContent = answer;
             }
         });
     });
 
-    backspace.addEventListener('click', () => {
-        if(num.toString().includes('.')) {
-            num = ((num * 10) - ((num * 10) % 10)) / 10;
-            display.textContent = `${num}.`;
-        }
-        //When number diplayed is followed by dot, remove the dot in the display
-        else if(display.textContent.charAt(display.textContent.length-1) == ".") {
-            display.textContent = display.textContent.substring(0, display.textContent.length-1);
-        }
-        else{
-            num = (num - (num % 10)) / 10;
-            display.textContent = num;
+    equals.addEventListener('click', () => {
+        if(operation == ''){  //if equals is clicked without operation selected
+            return;
         }
 
-        animateClick(backspace);
+        answer = operate(operation, firstNum, num);
+        
+        if(answer == "Syntax Error") {
+            display.textContent = answer;
+
+            operation = '';
+            firstNum = 0;
+            num = 0;
+            return;
+        }
+
+        operation = '';
+        firstNum = 0;
+        num = answer; // answer after clicking equal can still be used
+
+        //When there's a remainder, round to two decimal
+        if(answer%1 != 0 && answer != "Syntax Error"){
+            answer = answer.toFixed(2);
+        }
+
+        display.textContent = answer;
+    });
+
+    clear.addEventListener('click', () => {
+        operation = '';
+        firstNum = 0;
+        num = 0;
+
+        display.textContent = num;
     });
 });
